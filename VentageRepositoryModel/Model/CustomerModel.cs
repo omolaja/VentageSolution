6c28a903a2ffe0d718923b61ecf15b4de41034ab
omolaja <dipo142008@gmail.com>
2024-08-01 21:36:06 +0100

    Ref VRM in VR
    Referencing VentageRepositoryModel in VentageRepository

--- VentageRepositoryModel/Model/CustomerModel.cs
+++ VentageRepositoryModel/Model/CustomerModel.cs
@@ -25,13 +25,14 @@ namespace VentageRepositoryModel.Model
     {
         public int CustomerId { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "FirstName is required")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "LastName is required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string EmailAddress { get; set; }
 
         public string PhoneNumnber { get; set; }
